c0fcd3091ee4f29ce714d511b463735a3936ec3c
Anthony Salani <asalani93@gmail.com>
2015-11-05 10:40:09 -0500

    code coverage at 93%

--- lib/formula_fields/check.rb
+++ lib/formula_fields/check.rb
@@ -1,16 +1,27 @@
 module FormulaFields
-  def generate_dummy_env(parent, reqs, read_only = true)
+  def generate_dummy_env(parent, read_only, ns, reqs)
     env = Environment.new(read_only, parent)
 
     allowed_types = [:number, :text, :latitude, :longitude, :timestamp]
+    hash = {}
     reqs.keys.each do |name|
       type = reqs[name]
       val = if allowed_types.include? type
               get_nothing_type(type)
             elsif type.is_a? Array and type.length == 1
               ArrayType.new([], type[0])
+            else
+              TextType.new('a')
             end
-      env.add_ns name, val
+      if ns.nil?
+        env.add_ns name.to_s, val
+      else
+        hash[name.to_s] = val
+      end
+    end
+
+    unless ns.nil?
+      env.add_ns ns, hash
     end
 
     env
--- test/eval_test.rb
+++ test/eval_test.rb
@@ -18,41 +18,12 @@ class EvalTest < Minitest::Test
     ['./test/files/eval_fail.txt', 'fail']
   ]
 
-  FormulaFields.stdlib.add_ns '*', NumberType.new(1)
-
-  FormulaFields.stdlib.add_ns 'Test',
-    'a' => ArrayType.new([1, 2, 3, 4, 5], :number),
-    'b' => ArrayType.new(['a', 'b', 'c'], :text),
-    'c' => ArrayType.new([30, 60, 90, 120, 150], :latitude),
-    'd' => ArrayType.new([45, 90, 135, 180, 225, 270], :longitude),
-    'e' => ArrayType.new([
-      DateTime.new(2015, 1, 1, 1, 1, 1),
-      DateTime.new(2016, 1, 1, 1, 1, 1),
-      DateTime.new(2017, 1, 1, 1, 1, 1),
-      DateTime.new(2018, 1, 1, 1, 1, 1),
-      DateTime.new(2019, 1, 1, 1, 1, 1)
-    ], :timestamp),
-    'f' => ArrayType.new([1, 2, 3, 4, nil], :number),
-    'x1' => LongitudeType.new(75),
-    'x2' => LongitudeType.new(0),
-    'xn' => LongitudeType.new(nil),
-    'y1' => LatitudeType.new(0),
-    'y2' => LatitudeType.new(90),
-    'y3' => LatitudeType.new(75),
-    'yn' => LatitudeType.new(nil),
-    't1' => TimestampType.new('2015/8/24 9:31:00'),
-    't2' => TimestampType.new('2015/8/24 17:31:00'),
-    't3' => TimestampType.new('2015/8/25 9:31:00'),
-    't4' => TimestampType.new('2015/8/25 17:31:00'),
-    't5' => TimestampType.new('2015/8/26 13:41:32'),
-    'nn' => NumberType.new(nil)
-
   pass_files.each do |f|
     path = f[0]
     name = f[1]
     load_parse_test(path).each.with_index do |x, i|
       define_method("test_eval_#{name}_#{i}") do
-        curr_env = Environment.new(false, FormulaFields.stdlib)
+        curr_env = Environment.new(false, @env)
         src, res = x
         lex = Lexer.lex(src)
         parse = Parser.parse(src, lex)
@@ -67,7 +38,7 @@ class EvalTest < Minitest::Test
     name = f[1]
     load_parse_test(path).each.with_index do |x, i|
       define_method("test_eval_#{name}_#{i}") do
-        curr_env = Environment.new(false, FormulaFields.stdlib)
+        curr_env = Environment.new(false, @env)
         src, res = x
         begin
           lex = Lexer.lex(src)
@@ -80,5 +51,37 @@ class EvalTest < Minitest::Test
         assert_same_parse(src, error, unescape(res))
       end
     end
+  end
+
+  def setup
+    FormulaFields.stdlib.add_ns '*', NumberType.new(1)
+
+    @env = Environment.new(true, FormulaFields.stdlib)
+    @env.add_ns 'Test',
+      'a' => ArrayType.new([1, 2, 3, 4, 5], :number),
+      'b' => ArrayType.new(['a', 'b', 'c'], :text),
+      'c' => ArrayType.new([30, 60, 90, 120, 150], :latitude),
+      'd' => ArrayType.new([45, 90, 135, 180, 225, 270], :longitude),
+      'e' => ArrayType.new([
+        DateTime.new(2015, 1, 1, 1, 1, 1),
+        DateTime.new(2016, 1, 1, 1, 1, 1),
+        DateTime.new(2017, 1, 1, 1, 1, 1),
+        DateTime.new(2018, 1, 1, 1, 1, 1),
+        DateTime.new(2019, 1, 1, 1, 1, 1)
+      ], :timestamp),
+      'f' => ArrayType.new([1, 2, 3, 4, nil], :number),
+      'x1' => LongitudeType.new(75),
+      'x2' => LongitudeType.new(0),
+      'xn' => LongitudeType.new(nil),
+      'y1' => LatitudeType.new(0),
+      'y2' => LatitudeType.new(90),
+      'y3' => LatitudeType.new(75),
+      'yn' => LatitudeType.new(nil),
+      't1' => TimestampType.new('2015/8/24 9:31:00'),
+      't2' => TimestampType.new('2015/8/24 17:31:00'),
+      't3' => TimestampType.new('2015/8/25 9:31:00'),
+      't4' => TimestampType.new('2015/8/25 17:31:00'),
+      't5' => TimestampType.new('2015/8/26 13:41:32'),
+      'nn' => NumberType.new(nil)
   end
 end
